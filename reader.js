2a182d926058de5c01892e1799b0df3f3e846bde
Sota1221 <skobayashi009@berkeley.edu>
2021-10-23 19:00:35 -0700

    Add files via upload
    added ruler basis

--- reader.js
+++ reader.js
@@ -16,16 +16,45 @@ function changeWidth() {
   widthIndex = (widthIndex + 1) % widthList.length;
 }
 
+function toggleRuler() {
+  let
+}
+
 let fontSizeList = [100, 110, 120, 130, 140, 150, 160, 60, 70, 80, 90];
 let fontSizeIndex = 1;
 let x = 100;
+
+let lineHeightList = [140, 160, 180, 200, 220, 240, 260, 280, 300];
+let lineHeightIndex = 1;
+
+
 
 function changeFontSize() {
   document.getElementById("check").style.fontSize = fontSizeList[fontSizeIndex] + '%';
   fontSizeIndex = (fontSizeIndex + 1) % fontSizeList.length;
 }
 
+function changeLineHeight() {
+  document.getElementById("check").style.lineHeight = lineHeightList[lineHeightIndex] + '%';
+  lineHeightIndex = (lineHeightIndex + 1) % lineHeightList.length;
+}
+
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+console.log(open);
+console.log(modal_container);
+console.log(close);
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
